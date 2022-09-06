48493031ef3aaf85d997e87e88fd8b4d2be10993
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2022-09-06 13:55:24 -0700

    Boilerplate: useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,28 @@ import React, { useState } from 'react';
 
 function App() {
   console.log('Loading App comopnent....');
+
   /** 
    *  useState always returns an array, but we desctructure it for better readability
-   *  current state and function to update the state variable
+   *  - Initializing a state variable
+   *  const arr = useState(4);
+   *  const [count, setCount] = useState(4);
+   * 
+   * - Initializing a state variable using function
+   * 
   */
-  // const arr = useState(4);
-  // const [count, setCount] = useState(4);
-
-
-  /**
-   *  Initializing state variable using function and variable
-   */
-  const countInitialization = () => {
-    console.log('count initialization function');
-    return 4;
-  }
-
-  const [count, setCount] = useState(() => countInitialization());
 
   const decrementCount = () => {
-    setCount(count - 1);
-    // setCount(prevCount => prevCount - 1);
+    // TODO: Decrement count by 1. 
   }
 
   const incrementCount = () => {
-    // TODO: Increse the count by 1. 
+    // TODO: Increse count by 1. 
   }
   return (
     <div className="App">
       <button onClick={decrementCount}>-</button>
-      <h1>{count}</h1>
+      <h1>{/* TODO add Count */}</h1>
       <button>+</button>
     </div>
   );
