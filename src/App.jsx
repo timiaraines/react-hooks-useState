e291a3339348debd0489a7ac1a4dce8cd4256d7e
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2022-09-06 12:41:03 -0700

    React hooks useState

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  console.log('Loading App comopnent....');
+  /** 
+   *  useState always returns an array, but we desctructure it for better readability
+   *  current state and function to update the state variable
+  */
+  // const arr = useState(4);
+  // const [count, setCount] = useState(4);
+
+
+  /**
+   *  Initializing state variable using function and variable
+   */
+const countInitialization = () => {
+  console.log('count initialization function');
+  return 4;
+}
+
+  const [count, setCount] = useState(() => countInitialization());
+
+  const decrementCount = () => {
+    setCount(count - 1);
+    // setCount(prevCount => prevCount - 1);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={decrementCount}>-</button>
+      <h1>{count}</h1>
+      <button>+</button>
+    </div>
+  );
+}
+
+export default App;
